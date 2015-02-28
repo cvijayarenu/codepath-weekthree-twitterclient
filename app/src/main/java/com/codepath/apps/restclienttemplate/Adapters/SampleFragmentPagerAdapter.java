dbe2e5f9774948998f7a4e17b2553746c57d5ef5
chandrav <c@zoosk.com>
2015-02-28 08:30:26 -0800

    Added tabs.

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/Adapters/SampleFragmentPagerAdapter.java
@@ -0,0 +1,44 @@
+package com.codepath.apps.restclienttemplate.Adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.codepath.apps.restclienttemplate.Fragment.PageFragment;
+
+/**
+ * Created by chandrav on 2/28/15.
+ */
+public class SampleFragmentPagerAdapter extends android.support.v4.app.FragmentPagerAdapter {
+    final int PAGE_COUNT = 2;
+    private String tabTitles[] = new String[] { "Home", "Mentions" };
+
+    public SampleFragmentPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public int getCount() {
+        return PAGE_COUNT;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                
+                return PageFragment.newInstance(position + 1);
+            case 1:
+                return PageFragment.newInstance(position + 1);
+            default:
+                return PageFragment.newInstance(position + 1);
+        }
+        
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        // Generate title based on item position
+        return tabTitles[position];
+    }
+}
--- app/src/main/java/com/codepath/apps/restclienttemplate/Utils/TwitterClient.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/Utils/TwitterClient.java
@@ -67,6 +67,20 @@ public class TwitterClient extends OAuthBaseClient {
         
     }
     
+    public void getMentionsTimeline(Long max_id, AsyncHttpResponseHandler handler){
+        String apiUrl = getApiUrl("statuses/home_timeline.json");
+        RequestParams params = new RequestParams();
+        params.put("count", REST_TWEETS_COUNT);
+        params.put("include_rts", 1);
+        if(max_id != null){
+            params.put("max_id", max_id - 1);
+        }
+
+        getClient().get(apiUrl, params, handler);
+
+
+    }
+    
     
     public void composeTweet(AsyncHttpResponseHandler handler, String post){
         String apiUrl = getApiUrl("statuses/update.json");
